0bd8fdf9fb79d22f3a4ed51fdd64dfa3f413a616
Carlos Goce <carlosgoce@gmail.com>
2015-03-01 19:33:17 +0100

    Add named functions

--- test/FunctionFactsSpec.js
+++ test/FunctionFactsSpec.js
@@ -1,13 +1,19 @@
 describe("Functions", function() {
 
-  it("is assigned to window if is not assigned to a var", function() {
+  describe("Named", function() {
+    beforeEach(function() {
+      function isOnWindow() { return true; }
+    });
 
-      function isOnWindow() {
-          return true;
-      }
+    it("is assigned to window if is not assigned to a var", function() {
+      expect(typeof window.isOnWindow === "function");
+    });
 
-      expect(typeof window.isOnWindow === "function", "Function is defined");
-      expect(isOnWindow.name === "isOnWindow", "isOnWindow() has a name");
+    it("It has a name", function() {
+      function isOnWindow() { return true; }
+      
+      expect(isOnWindow.name === "isOnWindow");
+    });
   });
 
 });
