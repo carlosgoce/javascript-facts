4ea6947cd992a203fa0c73a81a2ce366af0a1745
Carlos Goce <carlosgoce@gmail.com>
2015-03-01 20:04:50 +0100

    Add more test cases for anonymous functions

--- test/FunctionFactsSpec.js
+++ test/FunctionFactsSpec.js
@@ -1,19 +1,30 @@
 describe("Functions", function() {
 
   describe("Named", function() {
-    beforeEach(function() {
-      function isOnWindow() { return true; }
-    });
-
+    function isOnWindow() { return true; }
+    
     it("is assigned to window if is not assigned to a var", function() {
       expect(typeof window.isOnWindow === "function");
     });
 
     it("It has a name", function() {
       function isOnWindow() { return true; }
-      
+
       expect(isOnWindow.name === "isOnWindow");
     });
+  });
+
+  describe("Anonymous", function() {
+    var anonymous = function() { return true; };
+
+    it("is a regular function", function() {
+        expect(typeof anonymous).toEqual("function");
+    });
+
+    it("has no name", function() {
+      expect(anonymous.name).toEqual("");
+    });
+
   });
 
 });
